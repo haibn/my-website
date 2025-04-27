194b05c3c9c5d252e5ee2755da87c8fc679ae7b6
haibn <haibinyuy@berkeley.edu>
2025-04-26 19:11:08 -0700

    hero section finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,49 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* Hero Section */}
+      <div className='h-screen w-full flex justify-center items-center'>
+        <div className='bg-red-200 w-[1024px] h-screen flex flex-col justify-between'>
+          
+          <div >
+            {/* Navbar */}
+            <nav className="flex items-center justify-between p-8">
+
+              {/* Logo */}
+              <div className="text-2xl font-bold">
+                <a href="/" className="font-medium text-5xl">h</a>
+              </div>
+
+              {/* Navbar links */}
+              <div className="space-x-10">
+                <a className="text-2xl hover:text-gray-400">work</a>
+                <a className="text-2xl hover:text-gray-400">about</a>
+                <a className="text-2xl hover:text-gray-400">contact</a>
+              </div>
+            </nav>
+          </div>
+
+          {/* My Name */}
+          <div className='bg-yellow-400 flex flex-col items-center w-full'>
+            <h3 className='w-full text-right font-medium text-[28px] bg-green-200 translate-y-[70%]'>web developer and designer</h3>
+            <h1 className="text-[329px] font-medium text-white leading-none -py-5 bg-blue-200">HAIBIN</h1>
+          </div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {/* Work Section */}
+      <div className='h-screen w-full flex justify-center items-center'>
+        <div className='bg-red-200 w-[1024px] h-screen flex flex-col justify-between'>
+
+          
+
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </>
   )
 }
